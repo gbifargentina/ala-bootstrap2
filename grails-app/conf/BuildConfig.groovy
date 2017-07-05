ad66e0d21b2c5e0a1553d272e2c36858ea21fbd2
Simon Bear <simon.bear@csiro.au>
2017-07-05 15:50:32 +1000

    Use latest ALA CAS library

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -23,7 +23,7 @@ grails.project.dependency.resolution = {
 
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-        compile ('au.org.ala:ala-cas-client:2.2') {
+        compile ('au.org.ala:ala-cas-client:2.3') {
             excludes([group: 'javax.servlet', name: 'servlet-api'])
         }
     }
