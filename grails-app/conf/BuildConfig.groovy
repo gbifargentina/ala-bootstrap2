51ccaf15ea62e82cba06e464569c755d83786b3a
Simon Bear <simon.bear@csiro.au>
2017-08-01 11:42:15 +1000

    Update BuildConfig.groovy
    Update to servlet 3.0, Java 7

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,7 +1,7 @@
-grails.servlet.version = "2.5"
+grails.servlet.version = "3.0"
 grails.project.work.dir = "target"
-grails.project.target.level = 1.6
-grails.project.source.level = 1.6
+grails.project.target.level = 1.7
+grails.project.source.level = 1.7
 
 grails.project.dependency.resolver = "maven"
 
